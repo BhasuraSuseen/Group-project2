29960b8ea87d892c86d169b50459700ea53eaf3e
BhasuraSuseen <bhasuras@live.com>
2016-09-13 14:31:42 +0530

    Diet update

--- diet.php
+++ diet.php
@@ -9,10 +9,10 @@
 require "connect.php";
 
 if(isset($_POST["submit"])){
-$sql = "INSERT INTO employee (NIC_NO, F_Name, L_Name, B_Date, Address, Contact_NO, Gender, E_Type, 
-FE_Date, EC_Date, FGP_Date, SGP_Date, CGP_Date, Salary, SI_Date, Remarks)
-VALUES ('{$_POST["nic"]}', '{$_POST["fname"]}', '{$_POST["lname"]}', '{$_POST["bdate"]}', '{$_POST["address"]}', '{$_POST["cnumber"]}', '{$_POST["gender"]}'
-, '{$_POST["etype"]}', '{$_POST["fedate"]}', '{$_POST["ecdate"]}', '{$_POST["fgpdate"]}', '{$_POST["sgpdate"]}', '{$_POST["cgpdate"]}', '{$_POST["salary"]}', '{$_POST["sidate"]}', '{$_POST["remarks"]}')";
+$sql = "INSERT INTO diet_counts (date, S1, S2, S3, Patient_B, Patient_L, Patient_D, Staff_B, 
+Staff_L, Staff_D, DD_B, DD_L, DD_D)
+VALUES ('{$_POST["date"]}', '{$_POST["subtype_1_count"]}', '{$_POST["subtype_2_count"]}', '{$_POST["subtype_3_count"]}', '{$_POST["subtype_1_pbreakfast"]}', '{$_POST["subtype_1_plunch"]}'
+, '{$_POST["subtype_1_pdinner"]}', '{$_POST["subtype_1_sbreakfast"]}', '{$_POST["subtype_1_slunch"]}', '{$_POST["subtype_1_sdinner"]}', '{$_POST["subtype_1_dbreakfast"]}', '{$_POST["subtype_1_dlunch"]}', '{$_POST["subtype_1_ddinner"]}')";
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
@@ -20,20 +20,17 @@ if (mysqli_query($conn, $sql)) {
 }
 }
 
-if(isset($_POST["search1"])){
-	
-	$sql1 = "SELECT * FROM employee WHERE F_Name ='{$_POST["search"]}'";
+    $sql1 = "SELECT S2 FROM menu_1 WHERE Item = 'milk'";
 	$result = mysqli_query($conn, $sql1);
-	
-	
-     //output data of each row
- if(mysqli_num_rows($result) > 0){
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<a class='employee_detail'> First Name : " . $row["F_Name"]. "<br>". "Last Name : " . $row["L_Name"]. "<br>". "Birth Date : " .$row["B_Date"]. "<br>". "Address : " .$row["Address"]. "<br>". "Contact NO : " .$row["Contact_NO"]. "<br>". "Gender : " .$row["Gender"]. "<br>". "NIC NO : " .$row["NIC_NO"]. "<br>". "Salary : " .$row["Salary"]. "<br></a>";
-    }
- }else{
+    
+    $milk;
+    if(isset($_POST["submit"])){        
+        $milk = $_POST["subtype_2_count"] * $result[0];
+        echo "<a> result : ".$result."</a><br>";
+        echo "<a> Milk : ".$milk."</a>";
+    
+}else{
    echo "0 results";
-}
 }
 
 
